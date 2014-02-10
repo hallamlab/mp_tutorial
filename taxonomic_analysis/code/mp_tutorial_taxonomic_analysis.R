1c5b179cdd2b3830b5af68ea13d4673304135d5a
Niels Hanson <nielshanson@me.com>
2014-02-09 18:53:39 -0800

    Added the HOT_megan_table.txt

--- taxonomic_analysis/code/mp_tutorial_taxonomic_analysis.R
+++ taxonomic_analysis/code/mp_tutorial_taxonomic_analysis.R
@@ -4,5 +4,13 @@
 # Tuesday, February 11, 2014
 # Niels W. Hanson (nielsh@mail.ubc.ca)
 
+# 1. Loading data
+# set the working directory to where you have the HOT_mean_table.txt
+setwd("~/Desktop/HOT")
+HOT_data <- read.table("HOT_megan_table.txt", header=TRUE, sep="\t", row.names=1)
 
 
+
+HOT_data.t <- t(HOT_data) # transpose HOT_data
+HOT_data.t.dist <- dist(HOT_data.t) # calculate the euclidian distance between the rows
+HOT_data.t.dist
