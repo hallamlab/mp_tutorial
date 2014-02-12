4f8b750ee717a6f2090b0fcc57952cbdc941bd99
Niels Hanson <nielshanson@me.com>
2014-02-11 23:41:35 -0800

    Updated R analysis slides

--- downstream_analysis_r/code/mp_tutorial_downstream_analysis.R
+++ downstream_analysis_r/code/mp_tutorial_downstream_analysis.R
@@ -56,7 +56,6 @@ pwys_770m <- rownames(pathways_wide)[pathways_wide[,"X7_omz"] > 0]
 pwys_4000m <- rownames(pathways_wide)[pathways_wide[,"X4_deepabyss"] > 0]
 
 # We can now use these as inputs to the our venn_diagram scripts
-# 
 venn_10m_and_4000m <- venn_diagram2(pwys_10m, pwys_4000m,
                                     "10m", "4000m")
 venn_10m_70m_130m <- venn_diagram3(pwys_10m, pwys_70m, pwys_130m,
@@ -71,188 +70,112 @@ venn_200m_500m_770m_4000m <- venn_diagram4(pwys_200m, pwys_500m, pwys_770m, pwys
 # it can also be valuable to compare interesting pathway sets from the above venn_diagrams
 # e.g. pathways common to 10m, 70m, and 130m, against pathways common to
 # 500m, 770m, and 4000m
-my_colors = c("#96B4D4", "#ED808B") # custom colors
-
-compare_cores <- venn_diagram2(venn_10m_70m_130m$"10m_70m_130m", venn_500m_770m_4000m$"500m_770m_4000m",
-              "Surface_Core", "Deep_Core", colors=my_colors)
+my_colors = c("#29A7A7", "#C000C0") # custom colors
+# combine function 'c' turns arguments into vectors
 
 # the euler option attempts to scale the relative sizes of the 
 # cirles (not always possible for more complex diagrams)
-venn_diagram2(venn_10m_70m_130m$"10m_70m_130m", venn_500m_770m_4000m$"500m_770m_4000m",
-            "Surface_Core", "Deep_Core", colors=my_colors, euler=TRUE)
-
-# Finally the out and file_name options allow you to write out a pdf file to the 
-# current working directory
-venn_diagram2(venn_10m_70m_130m$"10m_70m_130m", venn_500m_770m_4000m$"500m_770m_4000m",
-            "Surface_Core", "Deep_Core", colors=my_colors, out = TRUE, file_name = "my_filename", euler=TRUE)
+compare_cores <- venn_diagram2(venn_500m_770m_4000m$"500m_770m_4000m", venn_10m_70m_130m$"10m_70m_130m",
+                               "Deep", "Surface", colors=my_colors, euler=TRUE)
 
 ## 2. Hierarchcial Clustering, Heatmaps, Bubble Plots
 # load some required packages, otherwise install them and try again
-try( library("ecodist"), install.packages("ecodist") ) # Bray-Curtus dissimilarity
-library("ecodist")
-# Bootstrapped clustering
-source_url('http://raw.github.com/nielshanson/mp_tutorial/master/taxonomic_analysis/code/pvclust_bcdist.R')
 
-# Fit pvclust with bray-curtis distance and 10 bootstraps (in the interest of time)
-hist(as.matrix(pathways_wide))
-hist(as.matrix(log(pathways_wide+1)))
-pathways_wide_sqrt <- sqrt(pathways_wide)
-path_bcdist_sqrt.pvfit = pvclust(pathways_wide_sqrt, nboot=10, method.hclust="ward", method.dist="bray–curtis")
-plot(path_bcdist_sqrt.pvfit)
+# Data distributions
+par(mfrow=c(2,3))
+pathways_wide.matrix <- as.matrix(pathways_wide)
+hist(pathways_wide.matrix)
+hist(log(pathways_wide.matrix+1))
+hist(sqrt(pathways_wide.matrix))
+plot(density(pathways_wide.matrix))
+plot(density(log(pathways_wide.matrix+1)))
+plot(density(sqrt(pathways_wide.matrix)))
+par(mfrow=c(1,1))
 
-# we note that our clustering via pathways is not the same as when we did it with taxonomy
+# time to switch from wide to long table formats
+try( library("reshape2"), install.packages("reshape2") )
+library("reshape2")
 
-# visualization libraries ggplot2
-try( library("ggplot2"), install.packages("ggplot2") ) # Bubble plots
-library("ggplot2")
-try( library("RColorBrewer"), install.packages("RColorBrewer")) # Color palletes
-library("RColorBrewer")
+# add pathways from rowsnames to matrix
+pathways_wide$pwy = rownames(pathways_wide)
+# go from wide to long table format
+pathways_long <- melt(pathways_wide)
+colnames(pathways_long)[2] = "samp"
+pathways_long$samp <- sub("X","", pathways_long$samp)
 
+# add long pathway names 
+meta_17 <- read.table("../files/meta_17.txt", sep="\t", header=T, row.names=1)
+pathways_long$pwy_long = meta_17[pathways_long$pwy,1]
 
-# now that we are looking at the pathways directory it will help to have the translated pathway names
-# the MetaCyc hierarchy
-meta_17_hier <- read.table("ref/meta_17_hierarchy.txt", sep="\t", header=F, row.names=1)
-meta_17 <- read.table("ref/meta_17.txt", sep="\t", header=F, row.names=1)
+# download metacyc hierarchy & level the factors
+meta_17_hier <- read.table("../files/meta_17_hierarchy.txt", sep="\t", header=F, row.names=1)
 meta_17_hier$V3 <- factor(meta_17_hier$V3, levels=unique(meta_17_hier$V3))
 meta_17_hier$V4 <- factor(meta_17_hier$V4, levels=unique(meta_17_hier$V4))
 meta_17_hier$V5 <- factor(meta_17_hier$V5, levels=unique(meta_17_hier$V5))
 
-# another way to visualize a clustering is to plot a heatmap or bubble plot
-# there are many options and below is (in my opinion) a good set:
-# - sqrt transform: pathways tend to have lots of large and small values
-# - cluster on samples with bcdist (columns)
-# - Color Brewer Palette (http://colorbrewer2.org)
-# - remove the trance line
-# - outline of density black
-# - increase the margins to fit better
-
-# rownames(pathways_wide) <- as.vector(meta_17[order,1])
-# quartz()
-# heatmap.2(sqrt(as.matrix(pathways_wide[order,])), 
-#           distfun = bcdist,
-#           dendrogram = "col",
-#           col = brewer.pal(8, "Blues"),
-#           trace = "none",
-#           density.info = "histogram",
-#           denscol="black",
-#           margins = c(10,10),
-#           xlab="Samples",
-#           ylab="Pathways")
- 
-# to see all the colorbrewer paletes
-# quartz()
-# display.brewer.all()
- 
-# we might bring in ggplot at this point as a visualizaion framework
-# bubble plots which use bubble size rather than color have a better dynamic range 
-# than heatmaps
-
-# load pathways lookup table
-pathways_lookup <- read.table("data/HOT_pwys_lookup.txt", sep="\t", header=T)
+# add additional pathway levels 
+pwy_level1 <- meta_17_hier[pathways_long$pwy,2]
+pwy_level2 <- meta_17_hier[pathways_long$pwy,3]
+pathways_long <- cbind(pathways_long, pwy_level1)
+pathways_long <- cbind(pathways_long, pwy_level2)
 
 # small correction for pathways not found in hierarchy
-missing <- setdiff(intersect(pathways_lookup$PWY_NAME, pathways_lookup$PWY_NAME), rownames(meta_17_hier))
-pathways_lookup <- pathways_lookup[!(pathways_lookup$PWY_NAME %in% missing),]
+missing <- setdiff(unique(pathways_long$pwy), rownames(meta_17_hier))
+pathways_long <- pathways_long[!(pathways_long$pwy %in% missing),]
 
-pwy_order <- intersect(rownames(meta_17_hier[order(meta_17_hier[,"V3"], meta_17_hier[,"V4"]),]), unique(pathways_lookup$PWY_NAME))
-pathways_lookup$PWY_NAME <- factor(pathways_lookup$PWY_NAME, levels = pwy_order)
-pathways_lookup$SAMPLE <- factor(pathways_lookup$SAMPLE, levels = hot_metadata$sample)
+# order pathways first by level V3 and then by V4
+pwy_order <- intersect(rownames(meta_17_hier[order(meta_17_hier[,"V3"], meta_17_hier[,"V4"]),]), 
+                       unique(pathways_long$pwy))
 
-pwy_level1 <- meta_17_hier[pathways_lookup$PWY_NAME,2]
-pwy_level2 <-meta_17_hier[pathways_lookup$PWY_NAME,3]
+# factor pathways and then the samples 
+pathways_long$pwy <- factor(pathways_long$pwy, levels = pwy_order)
+pathways_long$samp <- factor(pathways_long$samp, levels = hot_metadata$sample)
 
-pathways_lookup <- cbind(pathways_lookup, pwy_level1)
-pathways_lookup <- cbind(pathways_lookup, pwy_level2)
-colnames(pathways_lookup)
+# whew, you got the longtable
 
-# rearrange according to clustering
-pathways_lookup$SAMPLE <- factor(pathways_lookup$SAMPLE, levels = path_bcdist.pvfit$hclust$labels[path_bcdist.pvfit$hclust$order])
-# cut clustering dedrogram to get groups
-cluster_groups <- cutree(path_bcdist.pvfit$hclust, h=0.35)
-pathways_lookup$cluster_group = cluster_groups[as.vector(pathways_lookup[,"SAMPLE"])]
-pathways_lookup$cluster_group = as.factor(pathways_lookup$cluster_group)
+# load ggplot2
+library(ggplot2)
 
-# bubble plots
-# decline and sum according to metacyc hierarchy
-quartz()
-g <- ggplot(subset(pathways_lookup, ORF_COUNT >0), aes(x=SAMPLE,y=pwy_level1)) + 
-     geom_point(aes(size=sqrt(ORF_COUNT), color=cluster_group)) + theme_bw() +
-     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) + 
-     labs(x = "Samples", y = "Pathways") 
-g
-
-quartz()
-g <- ggplot(subset(pathways_lookup, ORF_COUNT >0), aes(x=SAMPLE,y=pwy_level2)) + 
-     geom_point(aes(size=sqrt(ORF_COUNT), color=factor(cluster_group))) + theme_bw() +
+g <- ggplot(subset(pathways_long, value >0), aes(x=samp,y=pwy_level1)) +
+     geom_point(aes(size=sqrt(value), color=samp)) + theme_bw() + 
      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) + 
      labs(x = "Samples", y = "Pathways")
 g
 
-brew_colors = brewer.pal(8,"Blues")
-
-# heatmap in ggplot
-quartz()
-p <- ggplot(subset(pathways_lookup, ORF_COUNT >0), aes(SAMPLE, pwy_level2)) + 
-     geom_tile(aes(fill = sqrt(ORF_COUNT)), colour = "white") +
-     scale_fill_gradient(low = brew_colors[1], high=brew_colors[8]) + 
+g <- ggplot(subset(pathways_long, value >0), aes(x=samp,y=pwy_level2)) +
+     geom_point(aes(size=sqrt(value), color=samp)) + theme_bw() + 
      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) + 
      labs(x = "Samples", y = "Pathways")
-p
-
-# rearrange according to depth
-pathways_lookup$SAMPLE <- factor(pathways_lookup$SAMPLE, levels = hot_metadata$sample)
-
-# so many pathways might be interesting to see what is common between 10m, 70m, 130m
-pwys_10m_only <- subset(pathways_lookup, (PWY_NAME %in% compare_cores$Surface_Core_only))
-quartz()
-g <- ggplot(subset(pwys_10m_only, ORF_COUNT >0), aes(x=SAMPLE,y=PWY_COMMON_NAME)) + 
-     geom_point(aes(size=sqrt(ORF_COUNT))) +
-     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) + 
-     labs(x = "Samples", y = "Pathways") 
 g
 
-# so many pathways might be interesting to see what is common between 10m, 70m, 130m
-pwys_10m_only <- subset(pathways_lookup, (PWY_NAME %in% compare_cores$Deep_Core_only))
-quartz()
-g <- ggplot(subset(pwys_10m_only, ORF_COUNT >0), aes(x=SAMPLE,y=PWY_COMMON_NAME)) + 
-     geom_point(aes(size=sqrt(ORF_COUNT))) +
-     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) + 
-     labs(x = "Samples", y = "Pathways") 
+g <- ggplot(pathways_long, aes(pwy_level1)) +
+     geom_bar(aes(fill=samp)) + theme_bw() + coord_flip() + facet_wrap(~ samp, ncol = 7)
 g
 
-## 3. Dimentionality Reduction: PCA, NMDS
-library(ade4)
-library(vegan)
-library(gclus)
-library(ape)
+g <- ggplot(pathways_long, aes(pwy_level2)) +
+    geom_bar(aes(fill=samp)) + theme_bw() + coord_flip() + facet_wrap(~ samp, ncol = 7)
+g
 
-# Princple Component Analysis (PCA)
-# common to do a hellinger transform before PCA
-pathways_wide.h <- t(decostand(t(pathways_wide), "hellinger"))
-pathways_wide.h.pca <- rda(t(pathways_wide.h))
-p <- length(pathways_wide.h.pca$CA$eig)
-pathways_wide.h.pca.sc1 <- scores(pathways_wide.h.pca, display="wa", scaling=1, choices=c(1:p))
-quartz("Pathways: PCA")
-qplot(pathways_wide.h.pca.sc1[,1], pathways_wide.h.pca.sc1[,2], label=rownames(pathways_wide.h.pca.sc1), size=2, geom=c("point"), xlab="PC1 (25.1% Variance)", ylab="PC2 (19.5% Variance)", color=factor(cluster_groups)) + 
-geom_text(hjust=-0.1, vjust=0, colour="black", size=3) + theme_bw() + theme(legend.position="none") + xlim(-0.6, 0.6)
+g <- ggplot(pathways_long, aes(pwy_level2, fill=samp)) +
+     geom_bar() + theme_bw() + coord_flip()
+g
 
-# alternatively one can use the regular plots command
-quartz()
-par(cex=1.0, cex.lab=1.0, cex.axis=1.0, las=1, bty="n", pch=16)
-plot(pathways_wide.h.pca.sc1[,1], pathways_wide.h.pca.sc1[,2], col=cluster_groups, xlim=c(-0.7,0.4), ylim=c(-0.3,0.4), xlab="PC1", ylab="PC2")
-# 
+g <- ggplot(pathways_long, aes(value)) +
+     geom_density( aes(fill=samp, alpha=0.6)) + facet_wrap(~ samp, ncol=7) +
+     theme_bw() + theme(legend.position="none")
+g
 
+g <- ggplot(pathways_long, aes(log(value+1))) +
+     geom_density( aes(fill=samp, alpha=0.6)) + facet_wrap(~ samp, ncol=7) +
+     theme_bw() + theme(legend.position="none")
+g
 
-# Non-metric Multi-dimentional Scaling (NMDS)
+g <- ggplot(pathways_long, aes(value)) +
+     geom_histogram( aes(fill=samp)) + facet_wrap(~ samp, ncol=7) +
+     theme_bw() + theme(legend.position="none")
+g  
 
-pathways_wide.nmds <- metaMDS(t(pathways_wide), distance = "bray")
-
-quartz("Pathways NMDS - Bray")
-qplot(pathways_wide.nmds$points[,1], pathways_wide.nmds$points[,2], label=rownames(pathways_wide.nmds$points), size=2, geom=c("point"), 
-      xlab="MDS1", ylab="MDS2", main=paste("NMDS/Bray - Stress =", round(pathways_wide.nmds$stress,3)), color=factor(cluster_groups)) + 
-  geom_text(hjust=-0.1, vjust=0, colour="black", size=3) + theme_bw() +theme(legend.position="none") + xlim(-0.5,1.0)
-
-# alternatively one can always use the regular plots command
-quartz()
-par(cex=1.0, cex.lab=1.0, cex.axis=1.0, las=1, bty="n", pch=16)
-plot(pathways_wide.nmds$points[,1], pathways_wide.nmds$points[,2], col=cluster_groups, xlab="MDS1", ylab="MDS2", xlim=c(-0.4,0.8), ylim=c(-0.0002,0.0002))
+g <- ggplot(pathways_long, aes(log(value + 1))) +
+  geom_histogram( aes(fill=samp)) + facet_wrap(~ samp, ncol=7) +
+  theme_bw() + theme(legend.position="none")
+g
